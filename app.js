453a76521b42a3ab81c510be8491463c11d01562
e <126ying@sina.cn>
2018-10-10 00:58:30 +0800

    修改express方法

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express')
 var app = express()
-var ejs = require('ejs')
 var http = require('http')
 var path = require('path')
 
+// 设置引擎
+app.set("views", path.resolve(__dirname, "views"))
+app.set("view engine", "ejs")
 
+app.use(function(request, response) {  
+    response.writeHead(200, { "Content-Type": "text/plain" });      
+    response.end("Hello, World!");  
+})
+
+http.createServer(app).listen(3000)

8b90c79c828633fe0f074e4c2964a9a6cf8a4f56
yingjh <yjh.1234@qq.com>
2019-01-17 17:20:38 +0800

    修改项目结构

--- node/app.js
+++ node/app.js
@@ -10,8 +10,8 @@ app.set("views", path.resolve(__dirname, "views"))
 app.set("view engine", "ejs")
 
 app.use(function(request, response) {  
-    response.writeHead(200, { "Content-Type": "text/plain" });      
-    response.end("Hello, World!");  
+  response.writeHead(200, { "Content-Type": "text/plain" });      
+  response.end("Hello, World!");  
 })
 
 http.createServer(app).listen(3000)

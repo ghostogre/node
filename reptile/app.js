0f28dbd323b90139f0c7019634053c12bb3869ba
yingjh <yjh.1234@qq.com>
2019-01-18 18:03:06 +0800

    修改操作

--- reptile/app.js
+++ reptile/app.js
@@ -3,30 +3,28 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto('https://so.m.jd.com/webportal/channel/m_category?searchFrom=home');
-  await page.screenshot({path: 'example.png'});
-  // let titles = await page.$$('#category2 > li > a');
-  const _titles = await page.$$eval('#category2 > li > a', titles => {
-    return titles.map(item => {
-      return item.innerText
-    })
-  });
-  const array = _titles.map((item, index) => {
-    return {
-      title: item
-    }
-  })
-  // let array = await page.evaluate((...dom) => {
-  //   return dom.map((item) => {
-  //     return {
-  //       title: item.innerText
-  //     }
-  //   });
-  // }, ...titles);
+  let titles = await page.$$('#category2 > li > a');
+  // const _titles = await page.$$eval('#category2 > li > a', titles => {
+  //   return titles.map(item => {
+  //     return item.innerText
+  //   })
+  // });
+  let array = await page.evaluate((...dom) => {
+    return dom.map(async (item) => {
+      item.click();
+      await page.waitForRequest();
+      let navImg = await page.$eval('#branchList > div.jd-category-third-promotion > img', dom => dom.src)
+      return {
+        title: item.innerText,
+        nav_img: navImg
+      }
+    });
+  }, ...titles);
   fs.writeFile('data/category.json', JSON.stringify(array), 'utf8', () => {
     console.log('保存完成');
   })
-  await browser.close();
+  // await browser.close();
 })();
